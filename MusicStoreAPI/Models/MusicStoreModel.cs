e97eef6ea0cb85bd67ca98dbef9bbb4f6cedb4ba
ifotn <rich@infrontofthenet.com>
2017-12-20 12:16:40 -0500

    end of part 1.  model and app set up

---
+++ MusicStoreAPI/Models/MusicStoreModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStoreAPI.Models
+{
+    public class MusicStoreModel : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        public MusicStoreModel(DbContextOptions<MusicStoreModel> options ) : base(options)
+        {
+            
+        }
+    }
+}
